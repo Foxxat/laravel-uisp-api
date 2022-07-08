9beb98404a2aba5ca5a341f1773b73d6bd013e2e
Foxxat <Foxxat@users.noreply.github.com>
2022-07-08 23:44:44 +0000

    Fix styling

--- src/UISPApiClient.php
+++ src/UISPApiClient.php
@@ -18,6 +18,7 @@ class UISPApiClient
     public function getSites()
     {
         $sites = [];
+
         try {
             $unms_connection = new UISP($this->config['user'], $this->config['password'], $this->config['host'] . ':' . $this->config['port'], $this->config['verifyssl']);
             $debug_mode = $unms_connection->set_debug($this->config['debug']);
@@ -28,12 +29,14 @@ class UISPApiClient
         } catch (exception $e) {
             echo "Failed to connect to UISP Controller";
         }
+
         return $sites;
     }
 
     public function getDevices(string $id)
     {
         $devices = [];
+
         try {
             $unms_connection = new UISP($this->config['user'], $this->config['password'], $this->config['host'] . ':' . $this->config['port'], $this->config['verifyssl']);
             $debug_mode = $unms_connection->set_debug($this->config['debug']);
